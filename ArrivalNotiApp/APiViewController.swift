8b139ba1cad6cd451f72dfc3b9875c93b7261809
김주희 <juhee1687@naver.com>
2023-04-05 23:13:39 +0900

    create parsing data

--- ArrivalNotiApp/APiViewController.swift
+++ ArrivalNotiApp/APiViewController.swift
@@ -8,33 +8,217 @@
 import UIKit
 import Foundation
 
-class APiViewController: UIViewController {
+class APiViewController: UIViewController, XMLParserDelegate {
 
     
     @IBOutlet weak var inputBusRouteId: UITextField!
     
-    var url : String = "http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?ServiceKey=<your service key>&busRouteId="
+    var isLock = true
+    var tagType : ArrInfoByRouteAllData = .none
+    var tempModel: item?
+    var data: [item] = []
+    
+    var serviceKey : String = "your service key"
     
     override func viewDidLoad() {
         
     }
     
     func requestData(_ id: String) {
-        if let url = URL(string : url.appending(id)){
-            URLSession.shared.dataTask(with: url){ data, res, err in
-                if let data = data{
-                    
-                    let decoder = JSONDecoder()
-                
-                    print(String(decoding:data , as : UTF8.self))
-                    
-                }
-            }.resume()
-
+        var parser : XMLParser
+        var url = URL(string : "http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?ServiceKey=\(serviceKey)busRouteId=\(id)")
+        parser = XMLParser(contentsOf: url!)!
+        parser.delegate = self
+        parser.parse()
+    }
+    
+    // 태그 시작
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        if elementName == "item" {
+            isLock = true
+            tempModel = item.init()
+        } else if elementName == "arrmsg1" {
+            tagType = .arrmsg1
+        } else if elementName == "arrmsg2" {
+            tagType = .arrmsg2
+        } else if elementName == "arsId" {
+            tagType = .arsId
+        } else if elementName == "avgCf1" {
+            tagType = .avgCf1
+        } else if elementName == "avgCf2" {
+            tagType = .avgCf2
+        } else if elementName == "brdrde_Num2" {
+            tagType = .brdrde_Num2
+        } else if elementName == "brerde_Div1" {
+            tagType = .brerde_Div1
+        } else if elementName == "brerde_Div2" {
+            tagType = .brerde_Div2
+        } else if elementName == "busRouteAbrv" {
+            tagType = .busRouteAbrv
+        } else if elementName == "busRouteId" {
+            tagType = .busRouteId
+        } else if elementName == "busType1" {
+            tagType = .busType1
+        } else if elementName == "busType2" {
+            tagType = .busType2
+        } else if elementName == "deTourAt" {
+            tagType = .deTourAt
+        } else if elementName == "dir" {
+            tagType = .dir
+        } else if elementName == "expCf1" {
+            tagType = .expCf1
+        } else if elementName == "expCf2" {
+            tagType = .expCf2
+        } else if elementName == "exps1" {
+            tagType = .exps1
+        } else if elementName == "exps2" {
+            tagType = .exps2
+        } else if elementName == "firstTm" {
+            tagType = .firstTm
+        } else if elementName == "full1" {
+            tagType = .full1
+        } else if elementName == "full2" {
+            tagType = .full2
+        } else if elementName == "goal1" {
+            tagType = .goal1
+        } else if elementName == "goal2" {
+            tagType = .goal2
+        } else if elementName == "isArrive1" {
+            tagType = .isArrive1
+        } else if elementName == "isArrive2" {
+            tagType = .isArrive2
+        } else if elementName == "isLast1" {
+            tagType = .isLast1
+        } else if elementName == "isLast2" {
+            tagType = .isLast2
+        } else if elementName == "kalCf1" {
+            tagType = .kalCf1
+        } else if elementName == "kalCf2" {
+            tagType = .kalCf2
+        } else if elementName == "kals1" {
+            tagType = .kals1
+        } else if elementName == "kals2" {
+            tagType = .kals2
+        } else if elementName == "lastTm" {
+            tagType = .lastTm
+        } else if elementName == "mkTm" {
+            tagType = .mkTm
+        } else if elementName == "namin2Sec1" {
+            tagType = .namin2Sec1
+        } else if elementName == "namin2Sec2" {
+            tagType = .namin2Sec2
+        } else if elementName == "neuCf1" {
+            tagType = .neuCf1
+        } else if elementName == "neuCf2" {
+            tagType = .neuCf2
+        } else if elementName == "neus1" {
+            tagType = .neus1
+        } else if elementName == "neus2" {
+            tagType = .neus2
+        } else if elementName == "nextBus" {
+            tagType = .nextBus
+        } else if elementName == "nmain2Ord1" {
+            tagType = .nmain2Ord1
+        } else if elementName == "nmain2Ord2" {
+            tagType = .nmain2Ord2
+        } else if elementName == "nmain2Stnid1" {
+            tagType = .nmain2Stnid1
+        } else if elementName == "nmain2Stnid2" {
+            tagType = .nmain2Stnid2
+        } else if elementName == "nmain3Ord1" {
+            tagType = .nmain3Ord1
+        } else if elementName == "nmain3Ord2" {
+            tagType = .nmain3Ord2
+        } else if elementName == "nmain3Sec1" {
+            tagType = .nmain3Sec1
+        } else if elementName == "nmain3Sec2" {
+            tagType = .nmain3Sec2
+        } else if elementName == "nmain3Stnid1" {
+            tagType = .nmain3Stnid1
+        } else if elementName == "nmain3Stnid2" {
+            tagType = .nmain3Stnid2
+        } else if elementName == "nmainOrd1" {
+            tagType = .nmainOrd1
+        } else if elementName == "nmainOrd2" {
+            tagType = .nmainOrd2
+        } else if elementName == "nmainSec1" {
+            tagType = .nmainSec1
+        } else if elementName == "nmainSec2" {
+            tagType = .nmainSec2
+        } else if elementName == "nmainStnid1" {
+            tagType = .nmainStnid1
+        } else if elementName == "nmainStnid2" {
+            tagType = .nmainStnid2
+        } else if elementName == "nstnId1" {
+            tagType = .nstnId1
+        } else if elementName == "nstnId2" {
+            tagType = .nstnId2
+        } else if elementName == "nstnOrd1" {
+            tagType = .nstnOrd1
+        } else if elementName == "nstnOrd2" {
+            tagType = .nstnOrd2
+        } else if elementName == "nstnSec1" {
+            tagType = .nstnSec1
+        } else if elementName == "nstnSec2" {
+            tagType = .nstnSec2
+        } else if elementName == "nstnSpd1" {
+            tagType = .nstnSpd1
+        } else if elementName == "nstnSpd2" {
+            tagType = .nstnSpd2
+        } else if elementName == "plainNo1" {
+            tagType = .plainNo1
+        } else if elementName == "plainNo2" {
+            tagType = .plainNo2
+        } else if elementName == "rerdie_Div1" {
+            tagType = .rerdie_Div1
+        } else if elementName == "rerdie_Div2" {
+            tagType = .rerdie_Div2
+        } else if elementName == "reride_Num1" {
+            tagType = .reride_Num1
+        } else if elementName == "reride_Num2" {
+            tagType = .reride_Num2
+        } else if elementName == "routeType" {
+            tagType = .routeType
+        } else if elementName == "rtNm" {
+            tagType = .rtNm
+        } else if elementName == "sectOrd1" {
+            tagType = .sectOrd1
+        } else if elementName == "sectOrd2" {
+            tagType = .sectOrd2
+        } else if elementName == "stId" {
+            tagType = .stId
+        } else if elementName == "stNm" {
+            tagType = .stNm
+        } else if elementName == "staOrd" {
+            tagType = .staOrd
+        } else if elementName == "term" {
+            tagType = .term
+        } else if elementName == "traSpd1" {
+            tagType = .traSpd1
+        } else if elementName == "traSpd2" {
+            tagType = .traSpd2
+        } else if elementName == "traTime1" {
+            tagType = .traTime1
+        } else if elementName == "traTime2" {
+            tagType = .traTime2
+        } else if elementName == "vehId1" {
+            tagType = .vehId1
+        } else if elementName == "vehId2" {
+            tagType = .vehId2
+        } else {
+            tagType = .none
         }
-        else{
-            print("url is nil")
+    }
+    
+    // 태그 끝
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+            
         }
+    
+    // 태그 안의 내용
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
     }
     
     @IBAction func onActionCheck(_ sender: Any) {
--- ArrivalNotiApp/ArrInfoByRouteAllData.swift
+++ ArrivalNotiApp/ArrInfoByRouteAllData.swift
@@ -7,12 +7,269 @@
 
 import Foundation
 
-public class ArrInfoByRouteAllData : Codable {
-    
-    public var ServiceResult : String?
-    public var comMsgHeader : String?
-    public var headerCd : String?
-    public var headerMsg : String?
-    public var itemCount : String?
-
+enum ArrInfoByRouteAllData {
+    case arrmsg1
+    case arrmsg2
+    case arsId
+    case avgCf1
+    case avgCf2
+    case brdrde_Num1
+    case brdrde_Num2
+    case brerde_Div1
+    case brerde_Div2
+    case busRouteAbrv
+    case busRouteId
+    case busType1
+    case busType2
+    case deTourAt
+    case dir
+    case expCf1
+    case expCf2
+    case exps1
+    case exps2
+    case firstTm
+    case full1
+    case full2
+    case goal1
+    case goal2
+    case isArrive1
+    case isArrive2
+    case isLast1
+    case isLast2
+    case kalCf1
+    case kalCf2
+    case kals1
+    case kals2
+    case lastTm
+    case mkTm
+    case namin2Sec1
+    case namin2Sec2
+    case neuCf1
+    case neuCf2
+    case neus1
+    case neus2
+    case nextBus
+    case nmain2Ord1
+    case nmain2Ord2
+    case nmain2Stnid1
+    case nmain2Stnid2
+    case nmain3Ord1
+    case nmain3Ord2
+    case nmain3Sec1
+    case nmain3Sec2
+    case nmain3Stnid1
+    case nmain3Stnid2
+    case nmainOrd1
+    case nmainOrd2
+    case nmainSec1
+    case nmainSec2
+    case nmainStnid1
+    case nmainStnid2
+    case nstnId1
+    case nstnId2
+    case nstnOrd1
+    case nstnOrd2
+    case nstnSec1
+    case nstnSec2
+    case nstnSpd1
+    case nstnSpd2
+    case plainNo1
+    case plainNo2
+    case rerdie_Div1
+    case rerdie_Div2
+    case reride_Num1
+    case reride_Num2
+    case routeType
+    case rtNm
+    case sectOrd1
+    case sectOrd2
+    case stId
+    case stNm
+    case staOrd
+    case term
+    case traSpd1
+    case traSpd2
+    case traTime1
+    case traTime2
+    case vehId1
+    case vehId2
+    case none
 }
+
+struct item {
+    var arrmsg1: String?
+    var arrmsg2: String?
+    var arsId: String?
+    var avgCf1: String?
+    var avgCf2: String?
+    var brdrde_Num1: String?
+    var brdrde_Num2: String?
+    var brerde_Div1: String?
+    var brerde_Div2: String?
+    var busRouteAbrv: String?
+    var busRouteId: String?
+    var busType1: String?
+    var busType2: String?
+    var deTourAt: String?
+    var dir: String?
+    var expCf1: String?
+    var expCf2: String?
+    var exps1: String?
+    var exps2: String?
+    var firstTm: String?
+    var full1: String?
+    var full2: String?
+    var goal1: String?
+    var goal2: String?
+    var isArrive1: String?
+    var isArrive2: String?
+    var isLast1: String?
+    var isLast2: String?
+    var kalCf1: String?
+    var kalCf2: String?
+    var kals1: String?
+    var kals2: String?
+    var lastTm: String?
+    var mkTm: String?
+    var namin2Sec1: String?
+    var namin2Sec2: String?
+    var neuCf1: String?
+    var neuCf2: String?
+    var neus1: String?
+    var neus2: String?
+    var nextBus: String?
+    var nmain2Ord1: String?
+    var nmain2Ord2: String?
+    var nmain2Stnid1: String?
+    var nmain2Stnid2: String?
+    var nmain3Ord1: String?
+    var nmain3Ord2: String?
+    var nmain3Sec1: String?
+    var nmain3Sec2: String?
+    var nmain3Stnid1: String?
+    var nmain3Stnid2: String?
+    var nmainOrd1: String?
+    var nmainOrd2: String?
+    var nmainSec1: String?
+    var nmainSec2: String?
+    var nmainStnid1: String?
+    var nmainStnid2: String?
+    var nstnId1: String?
+    var nstnId2: String?
+    var nstnOrd1: String?
+    var nstnOrd2: String?
+    var nstnSec1: String?
+    var nstnSec2: String?
+    var nstnSpd1: String?
+    var nstnSpd2: String?
+    var plainNo1: String?
+    var plainNo2: String?
+    var rerdie_Div1: String?
+    var rerdie_Div2: String?
+    var reride_Num1: String?
+    var reride_Num2: String?
+    var routeType: String?
+    var rtNm: String?
+    var sectOrd1: String?
+    var sectOrd2: String?
+    var stId: String?
+    var stNm: String?
+    var staOrd: String?
+    var term: String?
+    var traSpd1: String?
+    var traSpd2: String?
+    var traTime1: String?
+    var traTime2: String?
+    var vehId1: String?
+    var vehId2: String?
+    
+    init() {
+     arrmsg1 = ""
+     arrmsg2 = ""
+     arsId = ""
+     avgCf1 = ""
+     avgCf2 = ""
+     brdrde_Num1 = ""
+     brdrde_Num2 = ""
+     brerde_Div1 = ""
+     brerde_Div2 = ""
+     busRouteAbrv = ""
+     busRouteId = ""
+     busType1 = ""
+     busType2 = ""
+     deTourAt = ""
+     dir = ""
+     expCf1 = ""
+     expCf2 = ""
+     exps1 = ""
+     exps2 = ""
+     firstTm = ""
+     full1 = ""
+     full2 = ""
+     goal1 = ""
+     goal2 = ""
+     isArrive1 = ""
+     isArrive2 = ""
+     isLast1 = ""
+     isLast2 = ""
+     kalCf1 = ""
+     kalCf2 = ""
+     kals1 = ""
+     kals2 = ""
+     lastTm = ""
+     mkTm = ""
+     namin2Sec1 = ""
+     namin2Sec2 = ""
+     neuCf1 = ""
+     neuCf2 = ""
+     neus1 = ""
+     neus2 = ""
+     nextBus = ""
+     nmain2Ord1 = ""
+     nmain2Ord2 = ""
+     nmain2Stnid1 = ""
+     nmain2Stnid2 = ""
+     nmain3Ord1 = ""
+     nmain3Ord2 = ""
+     nmain3Sec1 = ""
+     nmain3Sec2 = ""
+     nmain3Stnid1 = ""
+     nmain3Stnid2 = ""
+     nmainOrd1 = ""
+     nmainOrd2 = ""
+     nmainSec1 = ""
+     nmainSec2 = ""
+     nmainStnid1 = ""
+     nmainStnid2 = ""
+     nstnId1 = ""
+     nstnId2 = ""
+     nstnOrd1 = ""
+     nstnOrd2 = ""
+     nstnSec1 = ""
+     nstnSec2 = ""
+     nstnSpd1 = ""
+     nstnSpd2 = ""
+     plainNo1 = ""
+     plainNo2 = ""
+     rerdie_Div1 = ""
+     rerdie_Div2 = ""
+     reride_Num1 = ""
+     reride_Num2 = ""
+     routeType = ""
+     rtNm = ""
+     sectOrd1 = ""
+     sectOrd2 = ""
+     stId = ""
+     stNm = ""
+     staOrd = ""
+     term = ""
+     traSpd1 = ""
+     traSpd2 = ""
+     traTime1 = ""
+     traTime2 = ""
+     vehId1 = ""
+     vehId2 = ""
+    }
+    
+}
+
